229b48d51175a128961642a0e469fbb005b92ea2
gabigarrero <garrerogabriel@gmail.com>
2024-03-11 01:09:36 +0800

    Edited Login and Signup CSS files and Added Signup Page to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './login-page/login-page';
+import SignupPage from './signup-page/signup-page';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   );
