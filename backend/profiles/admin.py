b89519827a9635770c300bdc88a5f13346d08aca
Javi Ng <johnaidanvincent@gmail.com>
2024-03-24 22:12:04 +0800

    createed admins

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role']
+
+admin.site.register(UserProfile, ProfileAdmin)
