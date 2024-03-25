54a2ee622719993b68e84093bd4528f396f34f99
Javi Ng <johnaidanvincent@gmail.com>
2024-03-25 22:00:08 +0800

    various changes but mainly added parsers to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,6 +89,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ]
 }
 
 SIMPLE_JWT = {
