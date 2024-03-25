4b13188060e3654e72c2797355082c71c46f66bc
Andre Dalwin <andudtan@gmail.com>
2024-03-25 21:22:41 +0800

    Added additional fields for UserProfile model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -3,6 +3,10 @@ from authentication.models import CustomUser
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    mobile_number = models.CharField(max_length=13) # +63 9XX-XXX-XXXX
+    birthdate = models.DateField()
     profile_picture = models.ImageField(upload_to='profile_pictures/', default='default_profile_picture.jpg')
     viewed_recipes = models.JSONField(default=list, blank=True)
     user_recipe_ids = models.JSONField(default=list)
