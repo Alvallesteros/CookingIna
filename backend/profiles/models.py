06a81ef797e1703d0ebdfdaf774441445c539ad2
Javi Ng <johnaidanvincent@gmail.com>
2024-03-25 16:28:48 +0800

    added name and birthday to profile

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from authentication.models import CustomUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    mobile_number = models.CharField(max_length=13) # +63 9XX-XXX-XXXX
-    birthdate = models.DateField()
+    first_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True)
+    birthday = models.DateField(blank=True)
+    mobile_number = PhoneNumberField()
+    biography = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', default='default_profile_picture.jpg')
     viewed_recipes = models.JSONField(default=list, blank=True)
     user_recipe_ids = models.JSONField(default=list)
