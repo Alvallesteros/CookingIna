6ba6f5787662fa3051657793b30785f528da7d27
Javi Ng <johnaidanvincent@gmail.com>
2024-03-26 14:07:15 +0800

    added view for deleting profiles

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -7,32 +7,6 @@ from .models import UserProfile
 from .serializers import UserProfileSerializer
 from .permissions import IsOwner
 
-class UserProfileUpdateView(generics.UpdateAPIView):
-    # to update
-    # permission_classes = [IsAuthenticated, IsOwner]
-    queryset = UserProfile.objects
-    permission_classes = [permissions.AllowAny]
-    serializer_class = UserProfileSerializer
-    
-    def get_object(self):
-        username = self.kwargs.get('username')
-        user = CustomUser.objects.get(username=username)
-        return UserProfile.objects.get(user=user)
-
-    pass
-
-class UserProfileDetailView(generics.RetrieveAPIView):
-    queryset = UserProfile.objects
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.AllowAny]
-    
-    def get_object(self):
-        username = self.kwargs.get('username')
-        user = CustomUser.objects.get(username=username)
-        return UserProfile.objects.get(user=user)
-
-    pass
-
 class UserProfileCreateView(generics.CreateAPIView):
     queryset = UserProfile.objects
     serializer_class = UserProfileSerializer
@@ -49,3 +23,41 @@ class UserProfileCreateView(generics.CreateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserProfileDetailView(generics.RetrieveAPIView):
+    queryset = UserProfile.objects
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def get_object(self):
+        username = self.kwargs.get('username')
+        user = CustomUser.objects.get(username=username)
+        return UserProfile.objects.get(user=user)
+
+    pass
+
+class UserProfileUpdateView(generics.UpdateAPIView):
+    # to update
+    # permission_classes = [IsAuthenticated, IsOwner]
+    queryset = UserProfile.objects
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserProfileSerializer
+    
+    def get_object(self):
+        username = self.kwargs.get('username')
+        user = CustomUser.objects.get(username=username)
+        return UserProfile.objects.get(user=user)
+
+    pass
+
+class UserProfileDeleteView(generics.DestroyAPIView):
+    queryset = UserProfile.objects
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        username = self.kwargs.get('username')
+        user = CustomUser.objects.get(username=username)
+        return UserProfile.objects.get(user=user)
+    
+    pass
