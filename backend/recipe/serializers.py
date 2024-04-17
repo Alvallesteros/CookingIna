eb4fa20dbae077c4bb814af76c2acfc29cb54ec2
Andre Dalwin <andudtan@gmail.com>
2024-04-17 16:46:51 +0800

    Update serializer and views to implement create, update, and delete

--- backend/recipe/serializers.py
+++ backend/recipe/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Ingredient, Recipe
+from profiles.models import UserProfile
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,7 +8,7 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = '__all__'  
 
 from .models import Recipe, Ingredient
-from profiles.models import UserProfile
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)  # Nested Read-Only
@@ -16,3 +17,26 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = '__all__'
+
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)  
+        for ingredient_data in ingredients_data:
+            ingredient, _ = Ingredient.objects.get_or_create(**ingredient_data)
+            recipe.ingredients.add(ingredient)
+        return recipe
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr != 'ingredients':  
+                setattr(instance, attr, value)
+
+        ingredients_data = validated_data.pop('ingredients')
+        instance.ingredients.clear()  
+        for ingredient_data in ingredients_data:
+            ingredient, _ = Ingredient.objects.get_or_create(**ingredient_data)
+            instance.ingredients.add(ingredient)
+
+        instance.save()
+        return instance
--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions, status
-from rest_framework.response import Response
 from .models import Recipe
 from .serializers import RecipeSerializer
 
@@ -9,17 +8,40 @@ class RecipeListView(generics.ListCreateAPIView):
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]  
 
+    pass
+
 class RecipeDetailView(generics.RetrieveAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+    def get_object(self):
+        recipe_id = self.kwargs.get('recipe_id')
+        recipe = Recipe.objects.get(recipe_id=recipe_id)
+        return Recipe.objects.get(recipe=recipe)
+
+    pass
 
 class RecipeUpdateView(generics.UpdateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner, IsAdminUser]  
 
+    def get_object(self):
+        recipe_id = self.kwargs.get('recipe_id')
+        recipe = Recipe.objects.get(recipe_id=recipe_id)
+        return Recipe.objects.get(recipe=recipe)
+    
+    pass
+
 class RecipeDeleteView(generics.DestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner, IsAdminUser] 
+
+    def get_object(self):
+        recipe_id = self.kwargs.get('recipe_id')
+        recipe = Recipe.objects.get(recipe_id=recipe_id)
+        return Recipe.objects.get(recipe=recipe)
+    pass
