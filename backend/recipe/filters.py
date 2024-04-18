053f4250001eb27929f6f0588d04517d18e9019a
Javi Ng <johnaidanvincent@gmail.com>
2024-04-19 06:49:03 +0800

    fixed filters yay

--- backend/recipe/filters.py
+++ backend/recipe/filters.py
@@ -3,18 +3,18 @@ from .models import Recipe
 
 class RecipeFilter(filters.FilterSet):
     ingredients = filters.CharFilter(
-        name='ingredients__name',
-        lookup_expr='icontains',
+        field_name='ingredients__name',
+        lookup_expr='icontains'
     )
 
     cuisines = filters.CharFilter(
-        name='cuisines__name',
-        lookup_expr='icontains',
+        field_name='cuisine__name',
+        lookup_expr='icontains'
     )
 
-    title = filters.CharFilter(name='title', lookup_expr='icontains')
-    category = filters.CharFilter(name='category', lookup_expr='icontains')
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    category = filters.CharFilter(field_name='category', lookup_expr='icontains')
 
     class Meta:
         model = Recipe
-        fields = ('title', 'ingredients', 'cuisines', 'category')
+        fields = ('title', 'ingredients', 'cuisine', 'category')
