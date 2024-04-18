d836aa27bdda870794e7697dd6d6ab77204b97a9
Javi Ng <johnaidanvincent@gmail.com>
2024-04-19 06:54:51 +0800

    added more urls

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeUpdateView, RecipeDeleteView
+from .views import RecipeListView, RecipeDetailView, RecipeUpdateView, RecipeDeleteView, RecipeCreateView, IngredientListView, CuisineListView
 
 urlpatterns = [
     path('recipes/', RecipeListView.as_view(), name='recipe'),
+    path('recipes/create', RecipeCreateView.as_view(), name='recipe'),
     path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('recipes/<int:pk>/update/', RecipeUpdateView.as_view(), name='update-recipe'),
     path('recipes/<int:pk>/delete/', RecipeDeleteView.as_view(), name='delete-recipe'),
+    path('ingredients/', IngredientListView.as_view(), name='ingredients'),
+    path('cuisines/', CuisineListView.as_view(), name='cuisines'),
 ]
